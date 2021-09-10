a50dd1603cdb4f2c94590a36982ce73f05bebc8c
lilzj <joshua.gbogodor@decagon.dev>
2021-09-10 10:01:17 +0100

    refactored the models and made a new migration

--- EBook_Library.Data/EBookContext.cs
+++ EBook_Library.Data/EBookContext.cs
@@ -1,20 +1,14 @@
 ﻿using EBook_Library.Models;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EBook_Library.Data
 {
-    public class EBookContext : IdentityDbContext<AppUser>
+    public class EBookContext : DbContext
     {
         public EBookContext(DbContextOptions<EBookContext> options) : base(options)
         {
         }
-        public DbSet<AppUser> AppUsers { get; set; }
+      
         public DbSet<Book> Books { get; set; }
         public DbSet<BookActivity> BookActivities { get; set; }
     }
---
+++ EBook_Library.Data/Migrations/20210910085848_initial.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EBook_Library.Data.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    BookId = table.Column<string>(type: "TEXT", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", nullable: true),
+                    ISBN = table.Column<string>(type: "TEXT", nullable: true),
+                    PublishYear = table.Column<string>(type: "TEXT", nullable: true),
+                    CoverPrice = table.Column<decimal>(type: "TEXT", nullable: false),
+                    AvailabilityStatus = table.Column<bool>(type: "INTEGER", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DateModeified = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.BookId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BookActivities",
+                columns: table => new
+                {
+                    BookActivityId = table.Column<string>(type: "TEXT", nullable: false),
+                    BookId = table.Column<string>(type: "TEXT", nullable: true),
+                    FullName = table.Column<string>(type: "TEXT", nullable: true),
+                    Email = table.Column<string>(type: "TEXT", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "TEXT", nullable: true),
+                    NIN = table.Column<string>(type: "TEXT", nullable: true),
+                    PenaltyFee = table.Column<decimal>(type: "TEXT", nullable: false),
+                    NoOfDaysLate = table.Column<int>(type: "INTEGER", nullable: false),
+                    CheckOutDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CheckInDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ExpectedDateOfReturn = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookActivities", x => x.BookActivityId);
+                    table.ForeignKey(
+                        name: "FK_BookActivities_Books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Books",
+                        principalColumn: "BookId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookActivities_BookId",
+                table: "BookActivities",
+                column: "BookId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BookActivities");
+
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
